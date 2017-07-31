134480ceef0db22dee494b4b4b90604252ccaa7b
YangJiYong <milkcat1994@gmail.com>
2017-07-31 11:37:34 +0900

    jyYang's Layout submit

--- Layout_MarkDown/Notice_List.php
+++ Layout_MarkDown/Notice_List.php
@@ -1,11 +1,87 @@
-<?php
-<<!DOCTYPE html>
+<!--
+	made by Yang Ji Yong
+	made at 2017-07-30
+	View Notice_List
+-->
+
+<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="./Notice_List.css">
+	<title>공지사항</title>
 </head>
 <body>
+	<div id="main_border">
+		<div id="wrapper">
+			<div id="top_div_id">
+				<div class="top_div_class">
+					top
+				</div>
+			</div>
 
+			<div id="left_div_id">
+				<div class="left_div_class">
+					left
+				</div>
+			</div>
+
+			<div id="center_div_id">
+				<div class="center_div_class">
+					<div id="table_div_id">
+						<table>
+							<thead>
+							<tr>
+								<th>글번호</th>
+								<th>제목</th>
+								<th>작성자</th>
+								<th>조회수</th>
+								<th>작성일시</th>
+							</tr>
+							</thead>
+
+							<tfoot>
+								<tr>
+								</tr>
+							</tfoot>
+
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>title1</td>
+									<td>author1</td>
+									<td>1</td>
+									<td>2017-07-30</td>
+								</tr>
+								<tr>
+									<td colspan="5" class="table_line">
+										<img class="table_line" src="./table_line.png">
+									</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>title2</td>
+									<td>author1</td>
+									<td>10</td>
+									<td>2017-07-30</td>
+								</tr>
+								<tr>
+									<td colspan="5" class="table_line">
+										<img class="table_line" src="./table_line.png"></td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>title3</td>
+									<td>author2</td>
+									<td>1</td>
+									<td>2017-07-30</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
-</html>>
-?>
+</html>
