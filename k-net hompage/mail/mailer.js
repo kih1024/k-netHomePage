1611c0998daa4d441b68676a0ece36bca523d66d
YangJiYong <milkcat1994@gmail.com>
2017-10-27 19:29:51 +0900

    Uplaod ckeditor

--- k-net hompage/mail/mailer.js
+++ k-net hompage/mail/mailer.js
@@ -1,17 +1,20 @@
-var nodemailer = require('nodemailer');
+﻿var nodemailer = require('nodemailer');
 var rand = require('./createRandomString');
 
 
 var transporter = nodemailer.createTransport({
 	service: 'naver',
 	auth: {
+				//발송 이메일
 		user: 'rladlsgh654@naver.com', //임시 이메일
 		pass: '' //비밀번호
 	}
 });
 
-var mailerOptions = { //임시 이메일로 설정 
+var mailerOptions = { //임시 이메일로 설정
+//from과 발송 이메일 동일 
 	from: 'rladlsgh654@naver.com',
+//아래는 받는 이메일
 	to: 'kkkwon1008@naver.com',
 	subject: '메일발송입니다',
 	text: rand.randomStr()
